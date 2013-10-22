3205b10cee5ccc5825fa18d782bf709c13b8fbab
KennyLee <kennylee26@gmail.com>
2013-10-22 15:09:51 +0800

    完善用户绩效查询界面，管理员可跳转查看其他用户。

--- plugins/redmine_tk_redm/app/controllers/work_info_controller.rb
+++ plugins/redmine_tk_redm/app/controllers/work_info_controller.rb
@@ -17,13 +17,17 @@ class WorkInfoController < ApplicationController
   def user_detail
     require_login || return
     @user = User.current
-    if @user.admin and params[:id]!= nil then
-      uid = params[:id]
-      @users = User.all(:conditions => ["status = 1"])
+    if @user.admin and (params[:user_id]!= nil or params[:id]!=nil) then
+      uid = params[:user_id]
+      if (uid==nil)
+        uid = params[:id]
+      end
+      @users = User.all(:conditions => ['status = 1 and id not in (1,33,22,7,25,44,45,48)'], :order => 'firstname')
     else
       uid = @user.id
     end
-    remote_url = @@service_ip + '/ajax/user/work_info/'+uid.to_s+'?type=all';
+    @tid = uid.to_s
+    remote_url = @@service_ip + '/ajax/user/work_info/'+@tid+'?type=all';
 
     require 'net/http'
     require 'uri'
